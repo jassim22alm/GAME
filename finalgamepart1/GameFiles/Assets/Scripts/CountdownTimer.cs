abdbeec2b9734ac7508d8dcfc54f1bb827fac0c0
aanQMUL <ec20595@qmul.ac.uk>
2023-01-05 18:10:01 +0400

    Level 2 Countdown Timer
    Added count down timer when self destruct is activated. If timer hits 0, player loses

---
+++ finalgamepart1/GameFiles/Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CountdownTimer : MonoBehaviour
+{
+    public GameController gameController;
+    float currentTime = 0f;
+    float startingTime = 20f;
+    public GameObject uiObject;
+    public Text countdownText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiObject.SetActive(false);
+        currentTime = startingTime;
+        gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameController.selfDestruct == true) {
+            uiObject.SetActive(true);
+            currentTime -= 1 * Time.deltaTime;
+            countdownText.text = currentTime.ToString("0");
+
+            if (currentTime <= 0) {
+                SceneManager.LoadScene("GameOver");
+            }
+        }
+    }
+}
