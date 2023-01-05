25844ca200f82ccb8569e08f5db2d4210d97e010
aanQMUL <ec20595@qmul.ac.uk>
2023-01-05 11:49:59 +0400

    Door Scripts

---
+++ finalgamepart1/GameFiles/Assets/Scripts/Doors.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doors : MonoBehaviour
+
+{
+    public Animator door;
+    //public GameObject openText;
+    public bool inReach;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inReach = false;
+        door = this.transform.parent.GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = true;
+            //penText.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = false;
+            //openText.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inReach && Input.GetKeyDown(KeyCode.E))
+        {
+            DoorOpens();
+        }
+    }
+
+    void DoorOpens()
+    {
+        door.SetBool("character_nearby", true);
+    }
+
+
+}
