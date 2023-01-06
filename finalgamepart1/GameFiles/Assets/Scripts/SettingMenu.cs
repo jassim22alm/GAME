a0640fa76acafeef90fd661a77aa3a651e93b1ca
Chon Ho <carsonho02@gmail.com>
2023-01-06 20:56:07 +0800

    Setting Menu Updates

--- finalgamepart1/GameFiles/Assets/Scripts/SettingMenu.cs
+++ finalgamepart1/GameFiles/Assets/Scripts/SettingMenu.cs
@@ -6,7 +6,29 @@ using UnityEngine.Audio;
 
 public class SettingMenu : MonoBehaviour
 {
-    public AudioMixer mainMixer;//a reference to the audio mixer
+   public AudioMixer mainMixer;//a reference to the audio mixer
+
+   public Dropdown resolutionDropdown;// a reference to the resolution dropdown
+
+   Resolution[] resolutions;
+
+   void Start ()
+   {
+          resolutions = Screen.resolutions;
+
+          resolutionDropdown.ClearOptions();
+
+          List<string> options = new List<string>();
+
+          for (int i = 0; i < resolutions.Length; i++)
+          {
+                string option = resolutions[i].width + " x " + resolutions[i].height;
+                options.Add(option);
+          }
+
+          resolutionDropdown.AddOptions(options);
+   }
+
    public void SetVolume(float volume)// to increase or decrease the volume
    {
          mainMixer.SetFloat("volume",volume);
@@ -15,5 +37,10 @@ public class SettingMenu : MonoBehaviour
    public void SetQuality(int qualityIndex)// to game change the quality 
    {
           QualitySettings.SetQualityLevel(qualityIndex);
+   }
+
+   public void SetFullscreen (bool isFullscreen)// to set full screen
+   {
+          Screen.fullScreen = isFullscreen;
    }
 }
