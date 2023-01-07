f9667c1b0a5dd08f81b64ad2b15b9761f02d26d2
Chon Ho <carsonho02@gmail.com>
2023-01-07 15:22:23 +0800

    Setting Menu Updates

--- finalgamepart1/GameFiles/Assets/Scripts/SettingMenu.cs
+++ finalgamepart1/GameFiles/Assets/Scripts/SettingMenu.cs
@@ -10,23 +10,41 @@ public class SettingMenu : MonoBehaviour
 
    public Dropdown resolutionDropdown;// a reference to the resolution dropdown
 
-   Resolution[] resolutions;
+   Resolution[] resolutions;// the array for resolutions
 
    void Start ()
    {
+          // represent all the avaiable resolutions depends on the device
           resolutions = Screen.resolutions;
 
           resolutionDropdown.ClearOptions();
 
           List<string> options = new List<string>();
 
+          int currentResolutionIndex = 0;
+
           for (int i = 0; i < resolutions.Length; i++)
           {
                 string option = resolutions[i].width + " x " + resolutions[i].height;
                 options.Add(option);
+
+                // automatically set the device's default resolution
+                if (resolutions[i].width == Screen.currentResolution.width &&
+                    resolutions[i].height == Screen.currentResolution.height)
+                {
+                    currentResolutionIndex = i;
+                }
           }
 
           resolutionDropdown.AddOptions(options);
+          resolutionDropdown.value = currentResolutionIndex;
+          resolutionDropdown.RefreshShownValue();
+   }
+
+   public void SetResolution (int resolutionIndex)// to update the resolution
+   {
+          Resolution resolution = resolutions[resolutionIndex];
+          Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
    }
 
    public void SetVolume(float volume)// to increase or decrease the volume
